03ac88fb97aeb83691a6b4c77e52bcd87425d308
Jake Armijo <armijojake@yahoo.com>
2021-03-13 21:43:41 -0600

    updating

---
+++ .next/static/BBCwCpWvZLsVEWi4KbI-Z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-7f8bf83c4404c566233e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5a492c79e7c12bfd12e2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
