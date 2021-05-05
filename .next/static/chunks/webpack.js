cd9bf0194a3e40a11f3d504b6d4fad6f74d01627
Jake Armijo <armijojake@yahoo.com>
2021-05-05 16:37:09 -0500

    chore minor edits

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "323732ebdeee69659510";
+/******/ 	var hotCurrentHash = "930f35db7d22556a3b30";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
