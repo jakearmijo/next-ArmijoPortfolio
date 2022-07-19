8575ff1430975d72876133a4538b13af8ae9dc3f
Jake Armijo <armijojake@yahoo.com>
2022-07-19 07:04:16 -0600

    chore: adhd article rebuild

--- .next/server/pages/blog/[id].js
+++ .next/server/pages/blog/[id].js
@@ -252,7 +252,7 @@ module.exports = require("remark-html");
 var __webpack_require__ = require("../../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [61,246,445], () => (__webpack_exec__(3778)));
+var __webpack_exports__ = __webpack_require__.X(0, [61,377,445], () => (__webpack_exec__(3778)));
 module.exports = __webpack_exports__;
 
 })();
