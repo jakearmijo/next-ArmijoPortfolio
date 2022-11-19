0bcbfffd1337fab74fff92e11c529aca5f4dfc6f
Jake Armijo <armijojake@yahoo.com>
2022-11-19 10:14:37 -0700

    feat: trying for google analytics

--- pages/index.js
+++ pages/index.js
@@ -8,21 +8,32 @@ import Resume from "../components/Resume";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import DiscordWidget from "../components/DiscordWidget";
+import Script from 'next/script'
+
 
 
 function Home() {
+  
   return (
     <div className="main">
       <Head>
         <title>The Armijo Algorithm</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LLL7EVW69L"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments)}
-          gtag('js', new Date());
-          gtag('config', 'G-LLL7EVW69L');
-        </script>
+        <Script strategy="afterInteractive" async src="https://www.googletagmanager.com/gtag/js?id=G-LLL7EVW69L"></Script>
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LLL7EVW69L', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+        />
       </Head>
       <Navbar />
       <Hello />
